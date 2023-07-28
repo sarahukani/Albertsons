3d969d19ca9509a4e933c7a7c2fa079cf9839a87
yuvrajt463 <yuvrajt463@gmail.com>
2023-07-28 11:51:28 -0400

    updated to idris' more latest code, changed createPlaylist .js / .css

--- src/createPlaylistComp/createPlaylist.js
+++ src/createPlaylistComp/createPlaylist.js
@@ -1,154 +1,329 @@
 import React, { useState, useRef, useEffect } from "react";
+
 import html2pdf from "html2pdf.js";
+
+import { SketchPicker } from "react-color";
+
 import "./createPlaylist.css";
+
 import Gallery from "../uploadWidget/Gallery";
-import TextFieldsIcon from "@mui/icons-material/TextFields"; // Add this import
-import Icon from '../mainComp/Icon.js';
-import '../mainComp/Icon.css';
-import '../mainComp/Main.css';
-import Schedule from './Schedule.js';
-import './Schedule.css';
+
+import TextFieldsIcon from "@mui/icons-material/TextFields";
+
 
 
 function CreatePlaylist() {
-  const [imageSrc, setImageSrc] = useState(null);
-  const [selectedImages, setSelectedImages] = useState([]);
-  const [text, setText] = useState("");
-  const fileInputRef = useRef(null);
-  const [isPopupOpen, setIsPopupOpen] = useState(false); 
+
+    const [text, setText] = useState({ text: "", color: "#ffffff" });
+
+    const [showColorPicker, setShowColorPicker] = useState(false);
+
+    const [dragging, setDragging] = useState(false);
+
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    const [title, setTitle] = useState("Enter a title");
 
 
-  const handlePopupOpen = () => {
-    setIsPopupOpen(!isPopupOpen);
-    console.log(isPopupOpen)
-  };
+
+    const contentRef = useRef();
+
+    const textOverlayRef = useRef();
 
 
-  const handleImageUpload = (event) => {
-    const file = event.target.files[0];
-    const fileReader = new FileReader();
 
-    fileReader.onloadend = () => {
-      setImageSrc(fileReader.result);
+    const handleExport = () => {
+
+        const content = contentRef.current;
+
+
+
+        if (content && content.childNodes.length > 0) {
+
+            // Add the text overlay to the images
+
+            const textOverlay = document.createElement("div");
+
+            textOverlay.innerText = text.text;
+
+            textOverlay.style.position = "absolute";
+
+            textOverlay.style.top = `${position.y}px`;
+
+            textOverlay.style.left = `${position.x}px`;
+
+            textOverlay.style.transform = "translate(-50%, -50%)";
+
+            textOverlay.style.fontSize = "20px";
+
+            textOverlay.style.fontWeight = "bold";
+
+            textOverlay.style.color = text.color || "white"; // Set default color to white
+
+            textOverlay.style.textShadow = "1px 1px 2px rgba(0, 0, 0, 0.8)";
+
+
+
+            content.childNodes.forEach((child) => {
+
+                if (child.tagName === "IMG") {
+
+                    child.parentElement.appendChild(textOverlay.cloneNode(true));
+
+                }
+
+            });
+
+
+
+            html2pdf().from(content).save("exported_image.pdf");
+
+
+
+            const textOverlays = document.querySelectorAll(".image-item div");
+
+            textOverlays.forEach((overlay) => overlay.remove());
+
+        } else {
+
+            alert("Please add images to the gallery before exporting.");
+
+        }
+
     };
 
-    if (file) {
-      fileReader.readAsDataURL(file);
-    }
-  };
 
-  const handleExport = async () => {
-    if (imageSrc || selectedImages.length > 0) {
-      const content = document.createElement("div");
 
-      // Add the uploaded image if available
-      if (imageSrc) {
-        const imageElement = document.createElement("img");
-        imageElement.src = imageSrc;
-        imageElement.style.width = "100%";
-        content.appendChild(imageElement);
-      }
+    const handleColorChange = (color) => {
 
-      // Add selected images from the gallery
-      const galleryImages = document.querySelectorAll(
-        ".image-item.selected img"
-      );
-      const galleryImageElements = await Promise.all(
-        Array.from(galleryImages).map((img) => {
-          return new Promise((resolve, reject) => {
-            const imageElement = document.createElement("img");
-            imageElement.src = img.src;
-            imageElement.style.width = "100%";
-            imageElement.onload = () => resolve(imageElement);
-            imageElement.onerror = () => reject();
-          });
-        })
-      );
+        const newColor = `rgba(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b}, ${color.rgb.a})`;
 
-      galleryImageElements.forEach((img) => {
-        content.appendChild(img);
+        setText((prevText) => ({ ...prevText, color: newColor }));
 
-        // Add the text overlay to the images
-        const textOverlay = document.createElement("div");
-        textOverlay.innerText = text;
-        textOverlay.style.position = "absolute";
-        textOverlay.style.top = "50%";
-        textOverlay.style.left = "50%";
-        textOverlay.style.transform = "translate(-50%, -50%)";
-        textOverlay.style.fontSize = "20px";
-        textOverlay.style.fontWeight = "bold";
-        textOverlay.style.color = "white";
-        textOverlay.style.textShadow = "1px 1px 2px rgba(0, 0, 0, 0.8)";
-        img.parentElement.appendChild(textOverlay);
-      });
+    };
 
-      html2pdf().from(content).save("exported_image.pdf");
-    } else {
-      alert("Please upload an image or select images from the gallery.");
-    }
-  };
 
-  useEffect(() => {
-    if (selectedImages.length > 0) {
-      handleExport();
-    }
-  }, [text]);
 
-  const handleImageSelect = (imageUrl) => {
-    if (selectedImages.includes(imageUrl)) {
-      setSelectedImages(selectedImages.filter((image) => image !== imageUrl));
-    } else {
-      setSelectedImages([...selectedImages, imageUrl]);
-    }
-  };
+    const handleTextDragStart = (e) => {
 
-  return (
-    <div>
-      <div className="wrapper">
-      <div className="icon-container">
-      <Icon></Icon>
-      </div>
-      <div className="create-section">
-      <img
-          src="https://th.bing.com/th/id/R.86aff27675b3c44f1ba9bcef9e9ab268?rik=AjaxIZSY%2f%2b0pHQ&riu=http%3a%2f%2fthehealthgardener.com%2fwp-content%2fuploads%2f2018%2f11%2fcolour-wheel.png&ehk=d1uaGkz9InPVRYv%2b%2fZWTJTVTk%2fE%2bOtOIPj6NhxBJCww%3d&risl=&pid=ImgRaw&r=0"
-          alt="Color Wheel"
-          style={{ width: "30px", height: "30px", marginRight: "10px" }}
-        />
-        Independence day
-        <TextFieldsIcon
-          onClick={() => {
-            const textInput = window.prompt("Enter your text:");
-            setText(textInput);
-          }}
-        />
+        setDragging(true);
 
-        <button className="Schedulebtn" onClick={handlePopupOpen}>
-          Schedule Button
-        </button>
-        {isPopupOpen && (
-          <div className="popup">
-          <Schedule></Schedule>
-          </div>
+    };
 
-        )}
-      
 
-        <button className="Exportbtn" onClick={handleExport}>
-          Export
-        </button>
-      </div>
 
-      <div className="text-icon">
-        <div className="container">
-          <Gallery
-            selectedImages={selectedImages}
-            onImageSelect={handleImageSelect}
-          />
-          </div>
+    const handleTextDragEnd = (e) => {
+
+        setDragging(false);
+
+    };
+
+
+
+    const handleTextDrag = (e) => {
+
+        if (dragging) {
+
+            setPosition({ x: e.clientX, y: e.clientY });
+
+        }
+
+    };
+
+
+
+    const handleEnterTitle = () => {
+
+        const textInput = window.prompt("Enter your title:", title);
+
+        if (textInput !== null) {
+
+            setTitle(textInput);
+
+        }
+
+    };
+
+
+
+    // Clear images from localStorage on page refresh
+
+    useEffect(() => {
+
+        const clearImagesFromLocalStorage = () => {
+
+            localStorage.removeItem("galleryImages");
+
+        };
+
+        window.addEventListener("beforeunload", clearImagesFromLocalStorage);
+
+        return () => {
+
+            window.removeEventListener("beforeunload", clearImagesFromLocalStorage);
+
+        };
+
+    }, []);
+
+
+
+    return (
+
+        <div>
+
+            <div className="title">PAVILIONS</div>
+
+
+
+            <div className="create-section">
+
+                <div style={{ position: "relative" }}>
+
+                    <img
+
+                        src="https://th.bing.com/th/id/R.86aff27675b3c44f1ba9bcef9e9ab268?rik=AjaxIZSY%2f%2b0pHQ&riu=http%3a%2f%2fthehealthgardener.com%2fwp-content%2fuploads%2f2018%2f11%2fcolour-wheel.png&ehk=d1uaGkz9InPVRYv%2b%2fZWTJTVTk%2fE%2bOtOIPj6NhxBJCww%3d&risl=&pid=ImgRaw&r=0"
+
+                        alt="Color Wheel"
+
+                        style={{ width: "30px", height: "30px", marginRight: "10px" }}
+
+                        onClick={() => setShowColorPicker(!showColorPicker)}
+
+                    />
+
+                    {showColorPicker && (
+
+                        <div
+
+                            style={{
+
+                                position: "absolute",
+
+                                top: "100%",
+
+                                left: "0",
+
+                                zIndex: "1",
+
+                            }}
+
+                        >
+
+                            <SketchPicker
+
+                                color={text.color || "#ffffff"}
+
+                                onChange={handleColorChange}
+
+                            />
+
+                        </div>
+
+                    )}
+
+                </div>
+
+                <span onClick={handleEnterTitle} style={{ cursor: "pointer" }}>
+
+                    {title}
+
+                </span>
+
+                <TextFieldsIcon
+
+                    onClick={() => {
+
+                        const textInput = window.prompt("Enter your text:", text.text);
+
+                        if (textInput !== null) {
+
+                            setText((prevText) => ({ ...prevText, text: textInput }));
+
+                        }
+
+                    }}
+
+                />
+
+                <button className="Schedulebtn">Schedule</button>
+
+                <button className="Exportbtn" onClick={handleExport}>
+
+                    Export
+
+                </button>
+
+            </div>
+
+
+
+            <div
+
+                className="text-icon"
+
+                ref={contentRef}
+
+                onMouseUp={handleTextDragEnd}
+
+                onMouseMove={handleTextDrag}
+
+            >
+
+                <div className="container">
+
+                    <Gallery />
+
+                    {text.text && (
+
+                        <div
+
+                            ref={textOverlayRef}
+
+                            className="text-overlay"
+
+                            style={{
+
+                                position: "absolute",
+
+                                top: `${position.y}px`,
+
+                                left: `${position.x}px`,
+
+                                transform: "translate(-50%, -50%)",
+
+                                fontSize: "20px",
+
+                                fontWeight: "bold",
+
+                                color: text.color || "white",
+
+                                textShadow: "1px 1px 2px rgba(0, 0, 0, 0.8)",
+
+                                cursor: dragging ? "grabbing" : "grab",
+
+                            }}
+
+                            onMouseDown={handleTextDragStart}
+
+                        >
+
+                            {text.text}
+
+                        </div>
+
+                    )}
+
+                </div>
+
+            </div>
+
         </div>
-      </div>
-    </div>
-  );
+
+    );
+
 }
+
+
 
 export default CreatePlaylist;
