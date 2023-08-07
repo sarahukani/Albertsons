d4aaa201afbcd774348c8332603d7dc65a8ea3d2
yuvrajt463 <yuvrajt463@gmail.com>
2023-08-07 14:17:22 -0400

    reverted to gallery page working and images not showing up on create playlist, back button from gallery is still broken

--- src/uploadWidget/Gallery.js
+++ src/uploadWidget/Gallery.js
@@ -5,198 +5,208 @@ import EditIcon from '@mui/icons-material/Edit';
 import Uploader from '../uploadWidget/Uploader.js'
 import { useLocation } from 'react-router-dom';
 import Database from '../data/database';
+import DeleteIcon from '@mui/icons-material/Delete';
 
- 
- 
+
 
 const Gallery = ( props ) => {
-  const [images, setImages] = useState([]);
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [description, setDescription] = useState('');
-  const [showInput, setShowInput] = useState(false);
-  const [inputValue, setInputValue] = useState('');
-  const {state} = useLocation();
+const [images, setImages] = useState([]);
+const [selectedImage, setSelectedImage] = useState(null);
+const [description, setDescription] = useState('');
+const [showInput, setShowInput] = useState(false);
+const [inputValue, setInputValue] = useState('');
+const {state} = useLocation();
+const [isEditIconClicked, setIsEditIconClicked] = useState(false);
+const [imageInfo, setImageInfo] = useState('');
+const [editLink, setEditLink] = useState('');
 
- 
-    
-   let storeList = [];
-  
-  // console.log("Props.storeList", props.storeList);
-  // console.log("State.storeList", state.storeList);
-  if(!state.storeList) {
-    storeList = props.storeList;
-  } else{
-    storeList = state.storeList;
-  }
 
- 
+const location = useLocation();
+let storeIds = [];
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        console.log("This is storeList", storeList);
-        let products = await Database.getCurrentLocationProducts(storeList[0].id);
-        console.log('Fetched products:', products);
+if(location.state){
+    storeIds = location.state.storeIds;
+} else {
+    storeIds = props.storeIds;
+}
 
- 
 
-        // Extract the image_url from each product and create a new array
-        // const productImages = products.map((product) => product.image_url);
+let title = "Default";
+let price = "5.99";
 
- 
 
-        setImages(products);
-      } catch (error) {
-        console.error('Error fetching products:', error);
-      }
-    }
 
- 
 
-    fetchData();
-  }, [storeList]);
+useEffect(() => {
+async function fetchData() {
+try {
+console.log(storeIds);
+let products = await Database.getCurrentLocationProducts(storeIds[0].id);
+console.log('Fetched products:', products);
 
- 
 
-  const handleSelectIcon = (EditIcon) => {
-    setShowInput((prevShowInput) => !prevShowInput);
-  };
+// Extract the image_url from each product and create a new array
+// const productImages = products.map((product) => product.image_url);
 
- 
 
-  const handleImageSelect = (image) => {
-    // Toggle selection: if the clicked image is already selected, deselect it.
-    // Otherwise, set it as the selected image.
+setImages(products);
+} catch (error) {
+console.error('Error fetching products:', error);
+}
+}
 
- 
 
-    setSelectedImage((prevSelectedImage) =>
-      prevSelectedImage === image ? null : image
-    );
-    props.onSelectImage(image);
-  };
+fetchData();
+}, [storeIds]);
 
- 
 
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
+const handleSelectIcon = (EditIcon) => {
+setShowInput((prevShowInput) => !prevShowInput);
+};
 
- 
 
-  const handleSubmit = () => {
-    console.log('Submitting Description', inputValue, selectedImage);
+const handleImageSelect = (image) => {
+setImageInfo(image);
+// Toggle selection: if the clicked image is already selected, deselect it.
+// Otherwise, set it as the selected image
+setSelectedImage((prevSelectedImage) =>
+prevSelectedImage === image ? null : image
+);
+};
 
- 
 
-    const submittedDescription = inputValue;
-    console.log('Submitted Description', submittedDescription);
+const handleInputChange = (event) => {
+let tProduct = event.target.value;
+tProduct = tProduct.replaceAll(" ", "+");
+setInputValue(tProduct);
+};
 
- 
 
-    setDescription(submittedDescription);
-    setInputValue(''); // Clear the input after submission
-    setShowInput(false); // Hide the input container after submission
+const handleDeleteImage = (image) => {
+// Implement the logic to delete the selected image here
+// For example, you can use the image ID to delete it from the database
+console.log('Deleting image:', image);
+};
 
- 
 
-    const selectedIndex = images.findIndex((image) => image === selectedImage);
+async function handleSubmit() {
+try {
+setDescription(inputValue);
 
-  };
 
- 
+if (selectedImage) {
+const pID = imageInfo.id;
+const storeID = storeIds[0].id;
 
-  const handleInputClick = (event) => {
-    event.stopPropagation(); // Prevent the click event from bubbling up to the parent div (handleSelectIcon)
-  };
 
- 
+let editLink = await Database.getEditedImage(pID, title, price, inputValue, storeID);
+console.log(editLink);
+}
 
-  const labelStyle = {
-    marginBottom: '5px',
-  };
 
- 
+setInputValue(''); // Clear the input after submission
+setSelectedImage(null); // Deselect the image after submission
+window.location.reload();
+} catch (error) {
+console.error('Error submitting description:', error);
+}
+}
 
-  const inputStyle = {
-    padding: '5px',
-    marginBottom: '10px',
-  };
 
- 
 
-  let storeName = '';
-  let user = 'Rashmi';
-  if (state) {
-    storeName = state.storeName;
-    user = state.user;
-    storeList = state.storeList;
-  }
 
- 
 
-  return (
-<div className="gallery-container">
-      All uploads
+
+const handleInputClick = (event) => {
+event.stopPropagation(); // Prevent the click event from bubbling up to the parent div (handleSelectIcon)
+};
+
+
+const labelStyle = {
+marginBottom: '5px',
+};
+
+
+const inputStyle = {
+padding: '5px',
+marginBottom: '10px',
+};
+
+
+let storeName = '';
+let user = 'Rashmi';
+let storeList = [];
+if (state) {
+storeName = state.storeName;
+user = state.user;
+storeList = state.storeList;
+}
+
+
+return (
+<div className="gallery-container" >
+All upload
+<div className="italic-text">
+<br></br>
+To generate customized AI images, click on the selected image and type in your requirements.
+</div>
 <Icon storeName={storeName} storeList={storeList} user={user} />
 <div className="image-grid">
-        {images.map((image, index) => (
+{images.map((image, index) => (
 <div
-            className={`image-item ${image === selectedImage ? 'selected' : ''}`}
-            key={index}
-            onClick={() => handleImageSelect(image)}
-            style={{ position: 'relative' }}
+className={`image-item ${image === selectedImage ? 'selected' : ''}`}
+key={index}
+onClick={() => handleImageSelect(image)}
+style={{ position: 'relative' }}
 >
-            {/* Display the image */}
-            {image.imageURL ? (
+{/* Display the image */}
+{image.imageURL ? (
 <img src={image.imageURL} alt={image.name} />
-            ) : (
+) : (
 <div className="image-placeholder">Image Not Available</div>
-            )}
+)}
 
- 
 
-            {/* Display the description of the selected image */}
-            {image === selectedImage && (
+{/* Display the description of the selected image */}
+{image === selectedImage && (
 <div className="image-description">
 <p>{description}</p>
 </div>
-            )}
+)}
 
- 
 
-            {/* Show input container only for the selected image */}
-            {image === selectedImage && (
-<div className="containerStyle">
+
+
+{/* Show input container only for the selected image */}
+{image === selectedImage && (
+<div className="containerStyle" onClick={handleInputClick}>
 <label htmlFor="userInput" style={{ marginBottom: '5px' }}></label>
 <input
-                  type="text"
-                  id="userInput"
-                  value={inputValue}
-                  onChange={handleInputChange}
-                  style={{ padding: '5px', marginBottom: '10px' }}
-                />
+type="text"
+id="userInput"
+// value={inputValue}
+onChange={handleInputChange}
+style={{ padding: '5px', marginBottom: '10px' }}
+/>
 <button onClick={handleSubmit}>Submit</button>
 </div>
-            )}
+)}
 
- 
 
-            {/* Display the edit icon for all images */}
-            {image.imageURL && (
+{/* Display the edit icon for all images */}
+{image.imageURL && (
 <div className="edit-container">
 <div className="edit-button" onClick={handleSelectIcon}>
 <EditIcon />
 </div>
 </div>
-            )}
+)}
 </div>
-        ))}
+))}
 </div>
 </div>
-  );
+);
 };
 
- 
 
 export default Gallery;
