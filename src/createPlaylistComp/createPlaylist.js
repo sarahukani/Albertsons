6b0916c6dcf9db440ec69ec42b5b60ef872d29b0
yuvrajt463 <yuvrajt463@gmail.com>
2023-08-08 01:32:01 -0400

    save button responds with saved successfully

--- src/createPlaylistComp/createPlaylist.js
+++ src/createPlaylistComp/createPlaylist.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import html2canvas from 'html2canvas';
 import { SketchPicker } from 'react-color';
-import jsPDF from 'jspdf'; 
+import jsPDF from 'jspdf';
 import './createPlaylist.css';
 import Gallery2 from '../uploadWidget/Gallery2';
 import TextFieldsIcon from '@mui/icons-material/TextFields';
@@ -15,8 +15,8 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Alert from '@mui/material/Alert';
- 
- 
+
+
 const style = {
   position: 'absolute',
   top: '50%',
@@ -28,7 +28,7 @@ const style = {
   boxShadow: 24,
   p: 4,
 };
- 
+
 function CreatePlaylist() {
   const [title, setTitle] = useState('Enter a title');
   const [selectedGalleryItems, setSelectedGalleryItems] = useState([]);
@@ -41,83 +41,83 @@ function CreatePlaylist() {
   const [endDate, setEndDate] = useState(null);
   const [isScheduled, setIsScheduled] = useState(false);
   const { state } = useLocation();
- 
+
   console.log('This is our storelist prop that was passed in: ', state.storeList);
- 
+
   const handleClosePopup = () => {
     setPopupContent(false);
   };
- 
+
   const handlePopupOpen = () => {
     setPopupContent(true);
   };
- 
+
   const handleExportAndUpload = async () => {
     try {
       const content = textOverlayRef.current;
       // Capture the content as an image using html2canvas
       const canvas = await html2canvas(content, { scale: 2 });
       const imageData = canvas.toDataURL('image/jpeg', 1.0);
- 
+
       // Create a new jsPDF instance
       const pdf = new jsPDF('p', 'mm', 'a4');
- 
+
       // Add the image to the PDF
       pdf.addImage(imageData, 'JPEG', 10, 10, 190, 100);
- 
+
       // Get the text overlay elements and add them to the PDF
       const textOverlays = content.querySelectorAll('.text-overlay');
       const defaultFontSize = 12;
- 
+
       textOverlays.forEach((overlay) => {
         const text = overlay.innerText;
         const x = parseInt(overlay.style.left);
         const y = parseInt(overlay.style.top);
         pdf.text(text, x, y, { fontSize: defaultFontSize });
       });
- 
+
       // Save the PDF
       const pdfData = pdf.output('blob');
       const file = new File([pdfData], 'playlist.pdf', { type: 'application/pdf' });
       const fileList = [file];
       await Database.uploadProductImages(fileList);
- 
+
       console.log('PDF uploaded successfully!');
     } catch (error) {
       console.error('Error exporting and uploading PDF:', error);
     }
   };
- 
+  const [showSuccessModal, setShowSuccessModal] = useState(false);  
   const handleExport = () => {
     handleExportAndUpload();
   };
- 
+
   const handleEnterTitle = () => {
     const textInput = window.prompt('Enter your title:', title);
     if (textInput !== null) {
       setTitle(textInput);
     }
   };
- 
+
   const handleColorChange = (color) => {
     setTextColor(color.hex);
   };
- 
+
   const handleTextDragStart = (e) => {
     setTextPosition({ x: e.clientX, y: e.clientY });
   };
- 
+
   const handleTextDrag = (e) => {
     setTextPosition({ x: e.clientX, y: e.clientY });
   };
- 
+
   const handleSavePlaylist = async () => {
     if (!isScheduled) {
       // Show the modal if the playlist is not scheduled
       setOpenModal(true);
       return;
     }
- 
+    setShowSuccessModal(true);
     let playlist = {};
     try {
       const imageUrls = selectedGalleryItems.map((item) => item.imageURL);
@@ -128,7 +128,7 @@ function CreatePlaylist() {
     } catch (error) {
       console.error('Error while saving playlist:', error);
     }
- 
+
     try {
       const storeID = state.storeList[0].id;
       const playlistID = playlist.id;
@@ -137,26 +137,26 @@ function CreatePlaylist() {
       console.error('Error while linking playlist to store: ', error);
     }
   };
- 
+
   const enterTitleText = title === 'Enter a title' ? title : null;
- 
+
   // Modal-related state and handlers
   const [openModal, setOpenModal] = useState(false);
   const handleCloseModal = () => setOpenModal(false);
- 
+
   return (
     <div className="create-playlist-container">
-     <div className="gallery-section">
+      <div className="gallery-section">
         <Gallery2 storeList={state.storeList} onSelectImage={(image) => setSelectedGalleryItems([...selectedGalleryItems, { ...image, text: '' }])} />
       </div>
- 
+
       <div className="playlist-section">
         <div className="title">
           <span onClick={handleEnterTitle} style={{ cursor: 'pointer' }}>
             {enterTitleText}
           </span>
         </div>
- 
+
         <div className="create-section" style={{ background: 'black' }}>
           <div style={{ position: 'relative' }}>
             <img
@@ -165,7 +165,7 @@ function CreatePlaylist() {
               style={{ width: '30px', height: '30px', marginRight: '10px' }}
               onClick={() => setShowColorPicker(!showColorPicker)}
             />
- 
+
             {showColorPicker && (
               <div
                 style={{
@@ -179,11 +179,11 @@ function CreatePlaylist() {
               </div>
             )}
           </div>
- 
+
           <span className="titleName" onClick={handleEnterTitle} style={{ cursor: 'pointer', color: 'white' }}>
             {title !== 'Enter a title' && title}
           </span>
- 
+
           <TextFieldsIcon
             style={{ color: 'white' }}
             onClick={() => {
@@ -193,22 +193,22 @@ function CreatePlaylist() {
               }
             }}
           />
- 
+
           <button className="Schedulebtn" onClick={() => setPopupContent(true)}>
             Schedule
           </button>
- 
+
           <button className="Exportbtn" onClick={handleExport}>
             Export
           </button>
- 
+
           {selectedGalleryItems.length > 0 && (
             <button className="SavePlaylistbtn" onClick={handleSavePlaylist}>
               Save
             </button>
           )}
         </div>
- 
+
         <div className="text-icon" ref={textOverlayRef} onMouseDown={handleTextDragStart} onMouseMove={handleTextDrag}>
           {selectedGalleryItems.map((item) => (
             <div className="selected-gallery-image" key={item.id} style={{ position: 'relative', marginBottom: '16px' }}>
@@ -234,7 +234,7 @@ function CreatePlaylist() {
           ))}
         </div>
       </div>
- 
+
       {popupContent && (
         <div className="popup">
           <div className="popup-content">
@@ -252,7 +252,7 @@ function CreatePlaylist() {
           </div>
         </div>
       )}
- 
+
       <Modal
         aria-labelledby="modal-title"
         aria-describedby="modal-description"
@@ -267,10 +267,10 @@ function CreatePlaylist() {
         <Fade in={openModal}>
           <Box sx={style}>
             <Typography id="modal-title" variant="h6" component="h2">
-            {/* <Alert severity="info">This is an info alert — check it out!</Alert> */}
-            <Alert severity="error">This is an error alert — check it out!</Alert>
- 
- 
+              {/* <Alert severity="info">This is an info alert — check it out!</Alert> */}
+              <Alert severity="error">This is an error alert — check it out!</Alert>
+
+
               Your playlist is not saved!
             </Typography>
             <Typography id="modal-description" sx={{ mt: 2 }}>
@@ -280,8 +280,32 @@ function CreatePlaylist() {
           </Box>
         </Fade>
       </Modal>
+      {/* Modal for "Playlist successfully saved!" */}
+      <Modal
+        aria-labelledby="success-modal-title"
+        aria-describedby="success-modal-description"
+        open={showSuccessModal}
+        onClose={() => setShowSuccessModal(false)}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={showSuccessModal}>
+          <Box sx={style}>
+            <Typography id="success-modal-title" variant="h6" component="h2">
+              <Alert severity="success">This is a success alert — check it out!</Alert>
+              Playlist successfully saved!
+            </Typography>
+            <Typography id="success-modal-description" sx={{ mt: 2 }}>
+            </Typography>
+            <Button onClick={() => setShowSuccessModal(false)}>Close</Button>
+          </Box>
+        </Fade>
+      </Modal>
     </div>
   );
 }
- 
+
 export default CreatePlaylist;
